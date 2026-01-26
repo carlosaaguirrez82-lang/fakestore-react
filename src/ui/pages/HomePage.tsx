fad464daff275623b6043260753785ad810ab8f9
AndreaMacario-dev <andy.macariodelgado@gmail.com>
2026-01-26 16:52:05 -0600

    feat: creating  cards

--- src/ui/pages/HomePage.tsx
+++ src/ui/pages/HomePage.tsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import { 
+  Box,
+  Button, 
+  Card, 
+  CardActions,
+  CardContent,
+  CardMedia, 
+  Typography 
+} from '@mui/material' //todo componente
+import { useProducts } from '../../app/usecases/useProducts'
 
 const HomePage = () => {
+
+  const { data, isLoading, error } = useProducts()
+  
+    //todo crear un comopnente para loader
+     if (isLoading) return <p>Cargando...</p> 
+    //todo crear un componente para error 
+    if (error) return <p>Error al cargar productos</p>
+    //todo crear un componente para producto tipo card
+  /**
+   * * <Card product={product}/>**/
+
   return (
-    <div>HomePage</div>
+    <div>
+      <Box>
+        <div>Buscador *</div>
+        <div>Filtro por precio *</div>
+        <div>Filtro por rating *</div>
+      </Box>
+      <Box>
+        {data?.map((product) => (
+          <Card key={product.id} sx={{ maxWidth: 345 }}>
+            <CardMedia
+              sx={{ height: 140 }}
+              image="/static/images/cards/contemplative-reptile.jpg"
+              title="green iguana"
+            />
+            <CardContent>
+              <Typography>{product.title}</Typography>
+              <Typography>${product.price}</Typography>
+            </CardContent>
+            <Button>Ver más</Button>
+          </Card>
+        ))}
+      </Box>
+    </div>
   )
 }
 
