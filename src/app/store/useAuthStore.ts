b7500c6b6212f46535203bd18d4b835d48fe1b89
Andrea Macario <andrea.macario@it-seekers.com>
2026-01-25 20:26:18 -0600

    refactor: improve token security

--- src/app/store/useAuthStore.ts
+++ src/app/store/useAuthStore.ts
@@ -4,26 +4,32 @@ import { authApi } from '../../infrastructure/api/authApi'
 import type { LoginCredentials } from '../../domain/models/Auth';
 
 interface AuthState {
-    token: string | null
-    isAuthenticated: boolean
+    // Eliminamos el token del estado global por seguridad.
+    //token: string | null
+    isAuthenticated: boolean;
+    // Agregamos el user al estado global.
+    user: any | null;
     // Modificamos el método login para que use LoginCredentials
     // La función recibe un objeto credentials con username y password
     // usamos  Promise<void> porque es una función asíncrona que no devuelve nada, esto para que  el estado no guarde las credenciales.
-    login: (credentials: LoginCredentials) => Promise<void>
+    login: (credentials: LoginCredentials) => Promise<void>;
     //login: (username: string, password: string) => Promise<void>
-    logout: () => void
+    logout: () => void;
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
-    token: null,
+    //token: null,
+    user: null,
     isAuthenticated: false,
 
     login: async (credentials) => {
     // login: async (username, password) => {
     const { token } = await authApi.login(credentials.username, credentials.password);
     // const { token } = await authApi.login(username, password)
-    set({ token, isAuthenticated: true })
+    set({isAuthenticated: true });
+    localStorage.setItem('token', token);
     },
     
-    logout: () => set({ token: null, isAuthenticated: false }),
+    //logout: () => set({ token: null, isAuthenticated: false }),
+    logout: () => set({ isAuthenticated: false, user: null}),
 }))
