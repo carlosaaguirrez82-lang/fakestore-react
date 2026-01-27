5014867702cb0f0880166a9108631537c1127299
AndreaMacario-dev <andy.macariodelgado@gmail.com>
2026-01-27 08:49:38 -0600

    feat: importing product images

--- src/ui/pages/HomePage.tsx
+++ src/ui/pages/HomePage.tsx
@@ -13,34 +13,36 @@ const HomePage = () => {
 
   const { data, isLoading, error } = useProducts()
   
-    //todo crear un comopnente para loader
-     if (isLoading) return <p>Cargando...</p> 
-    //todo crear un componente para error 
-    if (error) return <p>Error al cargar productos</p>
-    //todo crear un componente para producto tipo card
+  //todo crear un comopnente para loader
+  if (isLoading) return <p>Cargando...</p> 
+  //todo crear un componente para error 
+  if (error) return <p>Error al cargar productos</p>
+  //todo crear un componente para producto tipo card
   /**
    * * <Card product={product}/>**/
 
   return (
     <div>
-      <Box>
+      <Box >
         <div>Buscador *</div>
         <div>Filtro por precio *</div>
         <div>Filtro por rating *</div>
       </Box>
-      <Box>
+      <Box sx={{ display: "flex", gap: 2, flexWrap: "wrap" }}>
         {data?.map((product) => (
           <Card key={product.id} sx={{ maxWidth: 345 }}>
             <CardMedia
               sx={{ height: 140 }}
-              image="/static/images/cards/contemplative-reptile.jpg"
-              title="green iguana"
+              component="img"
+              height="140"
+              image={product.image}
+              alt={product.title}
             />
             <CardContent>
               <Typography>{product.title}</Typography>
               <Typography>${product.price}</Typography>
             </CardContent>
-            <Button>Ver más</Button>
+            {/*<Button onclick={{handleProductDetail}}>Ver más</Button>*/}
           </Card>
         ))}
       </Box>
