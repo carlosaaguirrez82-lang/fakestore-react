3a6e3fa60461df9eff80efc707b2459a41927c49
AndreaMacario-dev <andy.macariodelgado@gmail.com>
2026-01-28 16:19:40 -0600

    feat: applying shopping cart counter

--- src/ui/components/AppBarComponent/ShoppingCart.tsx
+++ src/ui/components/AppBarComponent/ShoppingCart.tsx
@@ -1,8 +1,12 @@
 import { Button, IconButton, Badge } from '@mui/material';
 import LocalGroceryStoreIcon from '@mui/icons-material/LocalGroceryStore';
 import { useNavigate } from 'react-router-dom';
+import { useCartStore } from '../../../app/store/useCartStore';
 const ShoppingCart = () => {
 
+    const totalItems = useCartStore((s) =>
+      s.items.reduce((acc, item) => acc + item.quantity, 0)
+    )
   const navigate = useNavigate();
   const handleShoppingCart = () => {
     console.log('Carrito de compras clickeado');
@@ -15,7 +19,7 @@ const ShoppingCart = () => {
           aria-label="show 17 new notifications"
           color="inherit"
         >
-          <Badge>
+          <Badge badgeContent={totalItems} color="error">
             <LocalGroceryStoreIcon />
           </Badge>
     </IconButton>
